ec30513bf171167233a689cdfd6cba4e0f54be62
DIJITEA <57596699+DIJITEA@users.noreply.github.com>
2020-02-15 18:25:55 +0400

    push

---
+++ script.js
@@ -0,0 +1,14 @@
+function ryukswipe(){
+document.getElementById("playbutton1").classList.toggle("active");
+document.getElementById("playbutton1").classList.toggle("nonactive");
+document.getElementById("playbutton2").classList.toggle("active2");
+document.getElementById("playbutton2").classList.toggle("nonactive2");
+setTimeout(ryukswipe2, 200);
+function ryukswipe2(){
+document.getElementById("playbutton3").classList.toggle("active2");
+document.getElementById("playbutton3").classList.toggle("nonactive2");
+}
+document.getElementById("playbutton4").classList.toggle("active3");
+document.getElementById("playbutton4").classList.toggle("nonactive3");
+}
+
