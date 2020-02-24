3204e3a0a97f9fa50df27a2026074a97b8b59275
DIJITEA <57596699+DIJITEA@users.noreply.github.com>
2020-02-24 21:58:22 +0400

    update 24022020

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
+var Buttoncheck = 1;
+function playbutton(){
+  if (Buttoncheck == 1){
+  document.getElementById("hatbutton").classList.toggle("buttonposition");
+  document.getElementById("hatbutton").classList.toggle("revbuttonhide");
+  }
+}
 function ryukswipe(){
 document.getElementById("playbutton1").classList.toggle("active");
 document.getElementById("playbutton1").classList.toggle("nonactive");
 document.getElementById("playbutton2").classList.toggle("active2");
 document.getElementById("playbutton2").classList.toggle("nonactive2");
-setTimeout(ryukswipe2, 200);
-function ryukswipe2(){
-document.getElementById("playbutton3").classList.toggle("active2");
-document.getElementById("playbutton3").classList.toggle("nonactive2");
-}
-document.getElementById("playbutton4").classList.toggle("active3");
-document.getElementById("playbutton4").classList.toggle("nonactive3");
+setTimeout(playbutton, 100);
+
 }
 
-function test(){
-    alert("test");
+
+
+sessionStorage.clear('test');
+let realtimeval;
+function openmenu(){
+  Buttoncheck = 2;
+   realtimeval = val;
+
+  document.getElementById("sidebar" + realtimeval).classList.toggle("activesidebar");
+  document.getElementById("sidebar" + realtimeval).classList.toggle("nonactive");
+
+  let prevslideval = sessionStorage.getItem('test');
+  function second_phase(){
+if(prevslideval > 0 && prevslideval != realtimeval){
+
+  document.getElementById("sidebar" + prevslideval).classList.remove("activesidebar")
+  document.getElementById("sidebar" + prevslideval).classList.add("nonactive")
+}
+ryukswipe();
+  }
+setTimeout(second_phase, 300)
+
+sessionStorage.setItem('test', val); 
+
+}
+function ryukswipebacktime(){
+  document.getElementById("sidebar" + realtimeval).classList.toggle("activesidebar");
+  document.getElementById("sidebar" + realtimeval).classList.toggle("nonactive");
+}
+function ryukswipeback(){
+  if (Buttoncheck == 2){
+    ryukswipe();
+    setTimeout(ryukswipebacktime, 300)
+    
+    Buttoncheck = 3;
+  }
+  else if (Buttoncheck == 3){
+    Buttoncheck = 1;
+    ryukswipe();
+  }
+  else{
+    ryukswipe();
+  }
 }
