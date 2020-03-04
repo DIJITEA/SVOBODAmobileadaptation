b02e2b96e3d024745a98df8d6379a63b1d27beaa
DIJITEA <57596699+DIJITEA@users.noreply.github.com>
2020-03-04 23:25:50 +0400

    Fix0403202002

--- script.js
+++ script.js
@@ -40,32 +40,38 @@ function Homepagespawn1(){
 
 let magnet = window.innerHeight;
 
+
 window.addEventListener('scroll', function() {
-   
-    
     let cenwidth = window.innerHeight / window.innerHeight;
     let cent = window.innerHeight;
     let peny = pageYOffset;
     if(peny <= cent ){
-    document.getElementById('Centerspawn').style.opacity = (cenwidth - (peny / window.innerHeight) * 8);
-    document.getElementById('Wline1spawn').style.opacity = (cenwidth - (peny / window.innerHeight) * 8);
-    document.getElementById('Wline2spawn').style.opacity = (cenwidth - (peny / window.innerHeight) * 8);
-    document.getElementById('BG1').style.opacity = (cenwidth - (peny / window.innerHeight) * 2);
-    document.getElementById('BG2').style.opacity = (cenwidth - (peny / window.innerHeight) * 2);
-    
-    document.getElementById('About').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 16 - 5 + '%';
-    document.getElementById('Art').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 8 - 5  + '%';
-    document.getElementById('Piercing').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 4 - 5 + '%';
-    document.getElementById('Contacts').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 2 - 5 + '%';
-
-    document.getElementById('Gifmove').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 16 +  40 +'%';
-    document.getElementById('Gifmove2').style.top = ((cenwidth - (peny / window.innerHeight)) * 100) * 16 +  50 +'%';
-    document.getElementById('Gifmove3').style.top = ((cenwidth - (peny / window.innerHeight)) * 100) * (-8) +  60 +'%';
-
+        scrollfix();
     }
-   
+    else{
+        scrollfix();
+        peny = window.innerHeight; 
+    }
+    function scrollfix(){
+      
+        document.getElementById('Centerspawn').style.opacity = (cenwidth - (peny / window.innerHeight) * 8);
+        document.getElementById('Wline1spawn').style.opacity = (cenwidth - (peny / window.innerHeight) * 8);
+        document.getElementById('Wline2spawn').style.opacity = (cenwidth - (peny / window.innerHeight) * 8);
+        document.getElementById('BG1').style.opacity = (cenwidth - (peny / window.innerHeight) * 2);
+        document.getElementById('BG2').style.opacity = (cenwidth - (peny / window.innerHeight) * 2);
+        
+        document.getElementById('About').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 16 - 5 + '%';
+        document.getElementById('Art').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 8 - 5  + '%';
+        document.getElementById('Piercing').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 4 - 5 + '%';
+        document.getElementById('Contacts').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 2 - 5 + '%';
+    
+        document.getElementById('Gifmove').style.right = ((cenwidth - (peny / window.innerHeight)) * 100) * 16 +  40 +'%';
+        document.getElementById('Gifmove2').style.top = ((cenwidth - (peny / window.innerHeight)) * 100) * 16 +  50 +'%';
+        document.getElementById('Gifmove3').style.top = ((cenwidth - (peny / window.innerHeight)) * 100) * (-8) +  60 +'%';
+    
+      }
   });
-  
+
   setInterval(freedom,400);
   let Gifopt = 1;
   function freedom(){
